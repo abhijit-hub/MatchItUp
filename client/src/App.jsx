c7a137c953f9628f055add27614d8983e08e8a3e
abhijit-hub <itsabhijitmore@gmail.com>
2024-08-06 20:35:53 +0530

    changed structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -158,8 +158,8 @@ const App = () => {
   const saveRanking = async () => {
     try {
       const time = elapsedTime.seconds * 1000 + elapsedTime.milliseconds * 10;
-      await axios.post('/api/rankings', { time }); // Change here
-      const response = await axios.post('/api/calculateRank', { time }); // Change here
+      await axios.post('http://localhost:5000/api/rankings', { time, level: difficulty });
+      const response = await axios.post('http://localhost:5000/api/calculateRank', { time, level: difficulty });
       setUserRank(response.data.rank);
       fetchRankings();
       setIsModalOpen(true);
@@ -167,10 +167,10 @@ const App = () => {
       console.error('Error saving ranking:', error);
     }
   };
-  
+
   const fetchRankings = async () => {
     try {
-      const response = await axios.get('/api/rankings'); // Change here
+      const response = await axios.get('http://localhost:5000/api/rankings', { params: { level: difficulty } });
       setRankings(response.data);
     } catch (error) {
       console.error('Error fetching rankings:', error);
